e4d683a3c08c55d2d0b9b77711a044aa83f69f1f
Tejas Dhangar <tejasdhangar7pro@gmail.com>
2025-11-15 11:53:25 +0530

    Solved path error for serving home page

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/HTML/index.html"));
+  res.sendFile(path.join(__dirname, "public","HTML","index.html"));
 
 });
 
