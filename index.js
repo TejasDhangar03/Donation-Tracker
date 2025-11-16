63ca3391a1834511c92189ba6f4b687c6280b50d
Tejas Dhangar <tejasdhangar7pro@gmail.com>
2025-11-16 17:24:17 +0530

    removed static files

--- index.js
+++ index.js
@@ -21,10 +21,9 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,"public","index.html"));
-
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname,"public","index.html"));
+// });
 
 app.get('/data', async (req, res) => {
   try {
